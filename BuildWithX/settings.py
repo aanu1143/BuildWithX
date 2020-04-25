89847b2abaec7d701a5a017bcc9d8fcf7e899a6c
Anand Biswas <41153602+aanu1143@users.noreply.github.com>
2020-04-25 15:20:35 +0530

    setting

--- BuildWithX/settings.py
+++ BuildWithX/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .local_settings import SECRET_KEY, SENDGRID_PASS
+from .local_settings import SECRET_KEY, SENDGRID_PASS, DATABASE
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'BuildWithX.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': DATABASE
 }
 
 
