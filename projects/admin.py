22827fbedd93f4d21c8c013c70ac4a6d7298de31
gilbish <gilbeeshkosma15@gmail.com>
2020-05-08 12:27:54 +0530

    home page svg

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Project, Framework
 
 # Register your models here.
-admin.site.register(Project)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+	list_display = ('project_name','user')
+
 admin.site.register(Framework)
